de20f4cd1717cbccfac44e61a06d1fc040c57d95
Roman Predein <99061642+konopat@users.noreply.github.com>
2022-04-26 17:26:17 +0800

    Added comments

--- CountDownTimer.js
+++ CountDownTimer.js
@@ -1,15 +1,19 @@
+// Display result
 var display = document.getElementById("count");
 
+// The function is waiting count like number to be counted from and delay in milliseconds
 function startCountDown(count, delay) {
-  var timer = setInterval(function() {
+  var timer = setInterval(function() { // Declaring a timer
     if (count>0) {
-      display.textContent = count;
-      console.log(count);
-      count--;
+      // This will work until it reaches zero
+      display.textContent = count; // Displaying the current number
+      console.log(count); // Checking the value in the console
+      count--; // Decrease the number for the next iteration
     } else if (count == 0) {
-      display.textContent = 0;
-      console.log("Finished!");
-      clearInterval(timer);
+      // It will happen when it reaches zero
+      display.textContent = 0; // Displaying zero
+      console.log("Finished!"); // Checking the value in the console
+      clearInterval(timer); // Stopping the timer
     }
-  }, delay);
+  }, delay); // Declaring the timer delay
 }
